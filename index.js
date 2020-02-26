7577930d7b8b02b01921246b5803cb399d4fbf0d
piercecave <piercecave@yahoo.com>
2020-02-26 01:06:39 -0800

    points plotted and transition when clicked

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ const fetchAirbnbData = (nyGeoData) => {
 }
 
 const drawMap = (nyGeoData, airbnbData) => {
-    console.log(nyGeoData.features);
-    console.log(airbnbData);
 
-    var width = 500,
+    var width = 450,
         height = 450;
 
     var svg = d3.select("#mapContainer")
@@ -40,9 +38,38 @@ const drawMap = (nyGeoData, airbnbData) => {
         .enter()
         .append("path")
         .attr("fill", "#ccc")
+        .style("stroke", "black")
         .attr("d", geoPath);
 
-    g.attr("transform", "translate(-105, 210) rotate(-40)");
+    g.attr("transform", "translate(-105, 190) rotate(-40)");
+
+    g.selectAll("circle")
+        .data(airbnbData)
+        .enter()
+        .append("circle")
+        .attr("fill", "blue")
+        .attr('r', '2px')
+        .style("stroke", "#a9a9a9")
+        .style("stroke-width", ".5px")
+        .attr("cx", function (d) {
+            // console.log(albersProjection([d['longitude'], d['latitude']]))
+            return albersProjection([d['longitude'], d['latitude']])[0];
+        })
+        .attr("cy", function (d) {
+            return albersProjection([d['longitude'], d['latitude']])[1];
+        })
+        .on("click", function () {
+            d3.select(this)
+                .attr("opacity", 1)
+                .transition()
+                .duration(5000)
+                .attr("cx", width+200)
+                .attr("cy", height / 2)
+                .attr("opacity", 0)
+                .on("end", function () {
+                    d3.select(this).remove();
+                })
+        });
 }
 
 initMap();
